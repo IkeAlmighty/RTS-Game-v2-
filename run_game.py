be9a93af68b5374af111caafc180b87159bff921
Isaac Yates <isaac.c.yates-1@ou.edu>
2018-11-14 14:39:57 -0600

    minor changes across the board

--- run_game.py
+++ run_game.py
@@ -5,18 +5,19 @@ import pygame
 class Game(engine.Engine):
 
     def preload(self):
-        """"""
+        self.eventcache = engine.EventCache()
 
     def loop(self):
-        """"""
+        self.eventcache.update()
 
-        for event in pygame.event.get():
-            if event.type == pygame.KEYUP and event.key == pygame.K_ESCAPE:
-                self.running = False
+        if self.eventcache.key_up(pygame.K_ESCAPE):
+            self running = False
 
     def cleanup(self):
         """"""
 
+def main():
+    game = Game()
+    game.start([800, 600])
 
-game = Game()
-game.start([800, 600])
+main()
--- server-side/listening.py
+++ server-side/listening.py
@@ -1,5 +1,23 @@
+import noise, random
 
+class WorldInstance:
+    """
+    A world instance contains the seed for the immutable parts of
+    the world, and also contains all info for the mutable parts 
+    (entities) as well.
 
-class GameInstance:
+    Entity data is grouped and accessed by passing the unique entity 
+    id and the position of the entity to the world instance.
 
+    A world instance is randomely generated at first, 
+    but entities change over time in various ways, which is why
+    the WorldInstance class has its own update loop that calls
+    updates on chunks imbetween taking requests from users... IDK
+    """
+
+    def __init__(self, seed, size, square_width):
+        #place all the initial entities in the world using
+        #various noise functions
+        
     
+        
