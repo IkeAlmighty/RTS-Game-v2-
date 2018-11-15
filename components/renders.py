d0f069c87e2220b392d83551b957eac6fcf444ca
Isaac Yates <isaac.c.yates-1@ou.edu>
2018-11-15 16:23:27 -0600

    RenderGroup class, Buttons

---
+++ components/renders.py
@@ -0,0 +1,21 @@
+import pygame_boilerplate.engine as engine
+import pygame
+
+
+class RenderGroup:
+
+    def __init__(self):
+        self.__components = {}
+
+    def add(self, renderable_component, id):
+        self.__components[id] = renderable_component
+
+    def update(self):
+        for key in self.__components:
+            self.__components[key].update()
+
+    def get_by_id(self, id):
+        return self.__components[id]
+
+    def __iter__(self):
+        return self.__components.values().__iter__()
--- run_game.py
+++ run_game.py
@@ -1,17 +1,36 @@
 import pygame_boilerplate.engine as engine
-import pygame
+import pygame, components.renders
 
 
 class Game(engine.Engine):
 
     def preload(self):
-        self.eventcache = engine.EventCache()
+        self.ui = components.renders.RenderGroup()
+
+        self.ui.add(engine.Button(topleft=(0, 0), text="Quit"), "quit_button")
+        
+        button = engine.Button(text="HELLO")
+        button.rect.topleft = (self.screen_size[0] - button.rect.width, 0)
+        self.ui.add(button, "hello_button")
 
     def loop(self):
+        
+        #UPDATING:
         self.eventcache.update()
+        self.ui.update()
 
+
+        #CONTROL LOGIC:
         if self.eventcache.key_up(pygame.K_ESCAPE):
-            self running = False
+            self.running = False
+
+        if self.ui.get_by_id("quit_button").is_pressed():
+            self.running = False
+        
+
+        #RENDERING:
+        for button in self.ui:
+            self.render_later(button)
 
     def cleanup(self):
         """"""
